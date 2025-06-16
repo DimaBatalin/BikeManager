05a1b9e46e1f25c3d0e92f07ed76fe6b75fa76b1
DimaBatalin <125128471+DimaBatalin@users.noreply.github.com>
2025-06-16 23:35:35 +0500

    круто сделал

---
+++ README.md
@@ -0,0 +1,1 @@
+Установка модулей `pip install -r requirements.txt`
--- services/__init__.py
+++ services/__init__.py
@@ -2,5 +2,5 @@ from .storage import *
 from .reports import get_weekly_totals, get_monthly_totals
 
 __all__ = [
-    
+    get_weekly_totals, get_monthly_totals
 ]
